08efc4af057aa37ae976094b55720c5f806dc537
Jakub Pivoňka <me@jakubpivonka.cz>
2024-10-25 10:18:33 +0000

    Add setting for background color

--- src/Render/CanvasRender.ts
+++ src/Render/CanvasRender.ts
@@ -161,7 +161,7 @@ export class CanvasRender extends Render {
     }
 
     private clear(): void {
-        this.ctx.fillStyle = 'white';
+        this.ctx.fillStyle = this.setting.backgroundColor;
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
